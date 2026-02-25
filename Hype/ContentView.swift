2c3db6c6eb6288011154269430408fe2e773ee03
WTKstudios <Wtkstudios@gmail.com>
2026-02-25 00:06:46 +0000

    Initial Commit

---
+++ Hype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hype
+//
+//  Created by wtk.chiko on 25/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hype/HypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypeApp.swift
+//  Hype
+//
+//  Created by wtk.chiko on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
